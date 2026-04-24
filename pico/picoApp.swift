85a45f3e3549c83eff75b0a1fdac6d38f40c3866
Trong Tan Nguyen <trtnguyen1@gmail.com>
2026-04-25 08:17:29 +1000

    Initial Commit

---
+++ pico/picoApp.swift
@@ -0,0 +1,17 @@
+//
+//  picoApp.swift
+//  pico
+//
+//  Created by Tommy Nguyen on 25/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct picoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
